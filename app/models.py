39c20bdd878cb863e38d31823b3a15ced1a4b68a
Janice-M <janiceink001@gmail.com>
2019-08-05 11:47:25 +0300

    added tavern database

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from . import db
-from sqlalchemy.sql import func
 from . import login_manager
 from flask_login import UserMixin, current_user
 from werkzeug.security import generate_password_hash,check_password_hash
@@ -152,4 +151,5 @@ class Downvote(db.Model):
 
 
 
+    
     
