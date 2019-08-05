01d032e21aace95ea17a754a0e894a3673ce3b51
Janice-M <janiceink001@gmail.com>
2019-08-05 09:45:07 +0300

    r woosh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 export SECRET_KEY='Wamlambez69'
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:**kwasync@localhost/pitcherstarvern'
-export MAIL_USERNAME ='kokonutkoala@gmail.com'
-export MAIL_PASSWORD ='Bookdrive1234'
-export DATABASE_URL='postgresql+psycopg2://moringa:**kwasync@localhost/pitcherstarvern'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:**kwasync@localhost/moringa'
+export MAIL_USERNAME='kokonutkoala@gmail.com'
+export MAIL_PASSWORD='Bookdrive1234'
+export DATABASE_URL='postgresql+psycopg2://moringa:**kwasync@localhost/moringa'
 
 
 
