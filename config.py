0917a284324fa7db209f87fe455d934f57b63d8a
Janice-M <janiceink001@gmail.com>
2019-08-05 16:22:16 +0300

    email message

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    MAIL_SERVER = 'smtp.mail.google.com'
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
